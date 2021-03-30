8f72396096d06cf1276c4461a4128af4886cd1f3
Jake <jakenguyen0118@gmail.com>
2021-03-30 14:25:44 -0400

    Add project files.

---
+++ Type Conversion/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Type_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // one byte
+            // byte b = 1;
+            // 4 bytes
+            // int i = b;
+            // Console.WriteLine(i);
+
+            // chance for data loss because byte only goes up to 255
+            // int i = 1000;
+            // byte b = (byte)i;
+            // Console.WriteLine(b);
+
+            //try
+            //{
+            //    var number = "1234";
+            //    int i = Convert.ToByte(number);
+            //    Console.WriteLine(i);
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("The number could not be converted to a byte.");
+            //}
+
+            try
+            {
+                string str = "true";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
